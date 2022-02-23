6ec80e5aea20e32759b8c71cdc1d5ea834cad7d0
Marria <juanca_cruzi99@hotmail.com>
2022-02-22 21:40:03 -0300

    tucu commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -78,7 +78,11 @@ function menosQueNoventa(num) {
   // Devuelve "true" si el argumento de la función "num" es menor que noventa
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+  if(num < 90) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function mayorQueCincuenta(num) {
